ade1cbf0400e3686719b32ce9d3f3ad81258ad8d
Nitin <nitindv260@gmail.com>
2024-08-30 10:45:30 +0530

    first commit

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,7 +3,7 @@ import Cardbox from './components/Cardbox';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-400">
+    <div className="flex  ml-80 w-[40%] items-center justify-center h-screen bg-gray-400">
       <Cardbox/>
   </div>
   );
--- app/src/components/Cardbox.tsx
+++ app/src/components/Cardbox.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bike from '../assets/Image_bike.jpg';
 import play_icon from '../assets/image.png';
 
 const Cardbox = () => {
+    const [isExpand, setisExpand] = useState(false);
+
+    const handleButton =()=>{
+    setisExpand(!isExpand);
+    }
     return (
-        <div className='flex items-center p-4 bg-white shadow-lg rounded-lg max-w-sm'>
-                <img src={bike} alt='bike' className='w-16 h-16 border-solid rounded-lg' />
-                <div className='ml-4'>
-                    <h1 className='font-bold'>Tore Up</h1>
-                    <p className=''>Don Toliver</p>
-                </div>
-                <button className="ml-auto p-2 rounded-full">
-                <img src={play_icon} alt='play' className=' w-5 h-5' />
-                </button>
+        <div 
+        className= {isExpand ? 'flex-row items-center justify-between gap-3 p-5 bg-white shadow-lg rounded-lg max-w-md h-96' :
+        'flex items-center justify-between gap-3 p-5 bg-white shadow-lg rounded-lg max-w-screen-lg'}
+        >
+            <div className=''>
+            <img src={bike} alt='bike' className='w-16 h-16' />
             </div>
+            <div className=''>
+                <h1 className='font-bold'>Tore Up</h1>
+                <p className=''>Don Toliver</p>
+            </div>
+            <button onClick={handleButton} className="ml-28 p-2 rounded-full">
+                <img src={play_icon} alt='play' className=' w-5 h-5' />
+            </button>
+        </div>
     )
 }
 
